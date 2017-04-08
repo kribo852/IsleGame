4e3431044cc1fe0256ec3248648142af91e27bd3
Kristoffer Borg <kribo852@student.liu.se>
2017-04-08 07:58:54 +0000

    added night sky

--- Main.java
+++ Main.java
@@ -23,16 +23,19 @@ public static void main(String[] args){
 	Rainfall rainfall=new Rainfall();
 	
 	Graphics g=jframe.getGraphics(); 
-	BufferedImage sea=new BufferedImage(screensize,screensize,1);
+	BufferedImage sea[]=new BufferedImage[]{new BufferedImage(screensize,screensize,1)
+		,new BufferedImage(screensize,screensize,1),new BufferedImage(screensize,screensize,1)};
 	
-	makeSea(screensize, sea);
+	makeSea(screensize, sea[0]);
+	makeNightSkySea(screensize, sea[1]);
+	makeNightStormSea(screensize, sea[2]);
 	
 	BufferedImage drawbuffer=new BufferedImage(screensize,screensize,BufferedImage.TYPE_INT_ARGB);
 	Graphics drawbuffergraphics=drawbuffer.getGraphics();
 	
 	long millispassed=System.currentTimeMillis();
+	map.update();
 	
-	//
 	ShowCrafteables.setGrapihcs(g);
 	
 	while(true){
@@ -41,7 +44,8 @@ public static void main(String[] args){
 		boolean onland=map.update();
 		DayCycleClass.update();
 		
-		drawbuffergraphics.drawImage(sea, 0, 0, null);
+		drawbuffergraphics.drawImage(DayCycleClass.isDay() ? sea[0] : 
+		Rainfall.isActive()? sea[2] : sea[1], 0, 0, null);
 		map.paint(drawbuffergraphics, screensize, screensize);
 		
 		rainfall.paint(drawbuffergraphics,screensize,screensize);
@@ -79,9 +83,34 @@ public static void main(String[] args){
 	
 	private static void makeNightSkySea(int screensize, BufferedImage sea){
 		for(int j=0; j<screensize; j++){
-			double redbase=40;
-			double greenbase=30;
-			double bluebase=50;
+			double redbase=60;
+			double greenbase=50;
+			double bluebase=80;
+			double scale=(0.35*(screensize-j-1))/screensize;
+			scale+=1.0;
+		
+			Color c=new Color(Math.min(255 , (int)(scale*redbase)) , 
+			Math.min(255 , (int)(scale*greenbase)),  Math.min(255 , (int)(scale*bluebase)));
+		
+			for(int i=0; i<screensize; i++){
+				sea.setRGB(i, j, c.getRGB());
+			}
+		}
+		Random RND=new Random();
+		Graphics g=sea.getGraphics();
+		for(int i=0; i<5000; i++){
+			Color c=new Color(200,220,255,RND.nextInt(100));
+			g.setColor(c);
+			g.fillRect(RND.nextInt(screensize), RND.nextInt(screensize),2,2);
+		}
+		
+	}
+	
+	private static void makeNightStormSea(int screensize, BufferedImage sea){
+		for(int j=0; j<screensize; j++){
+			double redbase=45;
+			double greenbase=60;
+			double bluebase=75;
 			double scale=(0.35*(screensize-j-1))/screensize;
 			scale+=1.0;
 		
